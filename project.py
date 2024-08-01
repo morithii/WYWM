520a86ba4bdace031d4df0b5e8bc424830c094c9
morithii <biskichata1@gmail.com>
2024-08-02 00:31:54 +0100

    Knight project updated

--- project.py
+++ project.py
@@ -1,15 +1,17 @@
 knights_number = 0
+knights = []
 
 print("Let's create a knight!")
 name = input("What is the knight's name: ")
+knights.append(name)
 
 while knights_number < 1:
 
-    print("Their name is: " + name)
+    print("Their name is: " + knights[0])
     knights_number += 1
 
 print("--- What would you like to update? ---")
-print("1: Knight's name: " + name )
+print("1: Knight's name: " + knights[0] )
 
 try:
 
@@ -17,15 +19,15 @@ try:
     selection = int(input("Select your option: "))
 
     if selection  == 1:
-        if knights_number > 0:
+        if knights_number >= 0:
             print("You have a new knight!")
 
-        name = str(input("What is their new name: "))
-        print("Your knight's new name is: " + name)
+        knights[0] = str(input("What is their new name: "))
+        print("Your knight's new name is: " + knights[0])
     else:
         print("--- Please select a valid option ---")
 
 except:
     print("--- Try Again! ---")
 
-print("Goodbye sir " + name)
+print("Goodbye sir " + knights[0])
