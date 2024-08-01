dcba1b36ee86b4f1869e2da1c769cdfdfd6fb999
morithii <biskichata1@gmail.com>
2024-08-01 21:09:38 +0100

    wywm knight project, 1st commit

--- project.py
+++ project.py
@@ -1,16 +1,31 @@
+knights_number = 0
+
 print("Let's create a knight!")
 name = input("What is the knight's name: ")
-print("Their name is: " + name)
+
+while knights_number < 1:
+
+    print("Their name is: " + name)
+    knights_number += 1
 
 print("--- What would you like to update? ---")
 print("1: Knight's name: " + name )
 
-selection = int(input("Select your option: "))
+try:
 
-if selection  == 1:
-    name = str(input("What is their new name: "))
-    print("Your knight's new name is: " + name)
-else:
-    print("--- Please select a valid option ---")
+
+    selection = int(input("Select your option: "))
+
+    if selection  == 1:
+        if knights_number > 0:
+            print("You have a new knight!")
+
+        name = str(input("What is their new name: "))
+        print("Your knight's new name is: " + name)
+    else:
+        print("--- Please select a valid option ---")
+
+except:
+    print("--- Try Again! ---")
 
 print("Goodbye sir " + name)
