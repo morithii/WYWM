36d08f0cf650c8bebdb5eb36f193223d9133d5ec
morithii <biskichata1@gmail.com>
2024-08-18 20:46:35 +0100

    Update: revisiting the project

--- project.py
+++ project.py
@@ -1,33 +1,86 @@
-knights_number = 0
 knights = []
 
-print("Let's create a knight!")
-name = input("What is the knight's name: ")
-knights.append(name)
+# call for this when you want to create a new knight
+def create_knight():
+    knights_data = []
 
-while knights_number < 1:
+    print("Let's create a knight!")
 
-    print("Their name is: " + knights[0])
-    knights_number += 1
+    # Set the information up for the knight
+    knights_data = (str(input("What is the knight's name: ")))
 
-print("--- What would you like to update? ---")
-print("1: Knight's name: " + knights[0] )
+    # Adds the information to the knight
+    knights.append(knights_data)
 
-try:
+# Show the current knights and select one
+def select_knight(knights):
+    while knights_number < 1:
+
+        print("Their name is: " + knights[0])
+        knights_number += 1
+
+# Call a knight and change their data
+def change_data(knights):
+
+    print("--- What would you like to update? ---")
+    print("1: Knight's name: " + knights[0] )
+
+    try:
 
 
-    selection = int(input("Select your option: "))
+        selection = int(input("Select your option: "))
 
-    if selection  == 1:
-        if knights_number >= 0:
-            print("You have a new knight!")
+        if selection  == 1:
+            if knights_number >= 0:
+                print("You have a new knight!")
 
-        knights[0] = str(input("What is their new name: "))
-        print("Your knight's new name is: " + knights[0])
-    else:
-        print("--- Please select a valid option ---")
+            knights[0] = str(input("What is their new name: "))
+            print("Your knight's new name is: " + knights[0])
+        else:
+            print("--- Please select a valid option ---")
 
-except:
-    print("--- Try Again! ---")
+    except:
+        print("--- Try Again! ---")
 
-print("Goodbye sir " + knights[0])
+# This is the menu and we make our selections here
+def menu(knights_number):
+
+    # Print the display options
+    print("What do you want to do?")
+    print("1: Create a new knight")
+    print("0: Exit")
+
+    # Allos a selection to be tested
+    try:
+
+        # Takes the users selection option
+        select = int(input("Selection number: "))
+        print() # Creates a blank line
+
+        # Creates a new knight
+        if select == 1:
+            create_knight(knights)
+
+            #Print out knight that was made
+            print("\n--- Your Knight ---\n") #Alternative way to create new line
+            print("Knight'n name: " + str(knights[knights_number][0] + "\n"))
+            knights_number += 1
+            menu(knights_number)
+
+        elif select == 0:
+            print("Goodbye")
+
+        else:
+            print("--- Try Again! ---\n")
+            menu(knights_number)
+
+
+    except:
+        print("--- Try Again! ---")
+        menu(knights_number)
+
+# Setting the scene
+knights_number = 0
+
+
+menu(knights_number)
